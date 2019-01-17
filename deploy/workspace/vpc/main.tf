0f98f1ded37a9f9b1bf5e31a00dbc82bf00ecda8
David Poindexter <drpoindexter@gmail.com>
2019-01-17 17:39:10 -0500

    Document the vpc arguments

--- deploy/workspace/vpc/main.tf
+++ deploy/workspace/vpc/main.tf
@@ -1,3 +1,18 @@
+# @TODO: Document required/optional args
+
+#
+# -- Required Arguments --
+# cidr_block
+#
+# -- Optional Arguments --
+# instance_tenancy
+# enable_dns_support
+# enable_dns_hostnames
+# enable_classiclink
+# enable_classiclink_dns_support
+# assign_generated_ipv6_cidr_block
+# tags
+
 resource "aws_vpc" "vpc" {
   cidr_block = "${var.vpc_cidr[terraform.workspace]}"
 }
