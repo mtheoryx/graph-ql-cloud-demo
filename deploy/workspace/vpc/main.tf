3455d90fd6e89a3cbe0b0c2b47c6eec47bbbecf5
David Poindexter <drpoindexter@gmail.com>
2019-01-19 14:10:25 -0500

    Add public routing

--- deploy/workspace/vpc/main.tf
+++ deploy/workspace/vpc/main.tf
@@ -70,7 +70,26 @@ resource "aws_subnet" "private" {
   }
 }
 
-# routes
+# Public Routing
+resource "aws_route_table" "public-route-table" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  # tags
+}
+
+resource "aws_route" "public-route" {
+  route_table_id         = "${aws_route_table.public-route-table.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.igw.id}"
+}
+
+resource "aws_route_table_association" "public-route-assoc" {
+  route_table_id = "${aws_route_table.public-route-table.id}"
+  count          = "${length(split(",",var.private_subnets_cidr[terraform.workspace]))}"
+  subnet_id      = "${element(aws_subnet.public.*.id,count.index)}"
+}
+
+# private route
 # route table
 # route table associations
 # security group(s)
