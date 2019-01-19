f9a8f4a43c53fc680d28c3f8b77dc070b422558c
David Poindexter <drpoindexter@gmail.com>
2019-01-19 13:45:43 -0500

    Define IGW and all subnets

--- deploy/workspace/vpc/main.tf
+++ deploy/workspace/vpc/main.tf
@@ -16,9 +16,66 @@
 resource "aws_vpc" "vpc" {
   cidr_block = "${var.vpc_cidr[terraform.workspace]}"
 
+  # AZs
   tags {
     Name        = "The VPC"
     Environment = "${var.environment[terraform.workspace]}"
     Managed     = "Terraform v0.11.10"
   }
 }
+
+# igw
+# required
+# vpc_id
+#
+# optional
+# tags
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags {
+    Name        = "The IGW"
+    Environment = "${var.environment[terraform.workspace]}"
+    Managed     = "Terraform v0.11.10"
+  }
+}
+
+# public subnets (2)
+resource "aws_subnet" "public" {
+  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = "${element(split(",",var.public_subnets_cidr[terraform.workspace]),count.index)}"
+  availability_zone = "${element(split(",",var.azs[var.region]),count.index)}"
+  count             = "${length(split(",",var.public_subnets_cidr[terraform.workspace]))}"
+
+  # tags
+  tags {
+    Name        = "The Public Subnet(s)"
+    Environment = "${var.environment[terraform.workspace]}"
+    Managed     = "Terraform v0.11.10"
+  }
+}
+
+# private subnets (2)
+resource "aws_subnet" "private" {
+  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = "${element(split(",",var.private_subnets_cidr[terraform.workspace]),count.index)}"
+  availability_zone = "${element(split(",",var.azs[var.region]),count.index)}"
+  count             = "${length(split(",",var.private_subnets_cidr[terraform.workspace]))}"
+
+  # tags
+  tags {
+    Name        = "The Private Subnet(s)"
+    Environment = "${var.environment[terraform.workspace]}"
+    Managed     = "Terraform v0.11.10"
+  }
+}
+
+# routes
+# route table
+# route table associations
+# security group(s)
+# security group(s) rule(s)
+# bastion
+# EIP for bastion (caution, free only while attached)
+# keys (tf-controlled, generated keys)
+
--- deploy/workspace/vpc/variables.tf
+++ deploy/workspace/vpc/variables.tf
@@ -1,3 +1,7 @@
+variable "region" {
+  default = "us-east-2"
+}
+
 variable "account_roles" {
   type        = "map"
   description = "Roles to assume per environment"
@@ -15,6 +19,35 @@ variable "vpc_cidr" {
   default = {
     dev  = "10.0.0.0/16"
     prod = "10.1.0.0/16"
+  }
+}
+
+variable "public_subnets_cidr" {
+  type        = "map"
+  description = "Public subnet cidr blocks by environment"
+
+  default = {
+    dev  = "10.0.1.0/24, 10.0.2.0/24"
+    prod = "10.1.1.0/24, 10.1.2.0/24"
+  }
+}
+
+variable "private_subnets_cidr" {
+  type        = "map"
+  description = "Private subnet cidr blocks by environment"
+
+  default = {
+    dev  = "10.0.3.0/24,10.0.4.0/24"
+    prod = "10.1.3.0/24,10.1.4.0/24"
+  }
+}
+
+variable "azs" {
+  type        = "map"
+  description = "Available AZs per region"
+
+  default = {
+    us-east-2 = "us-east-2a,us-east-2b,us-east-2c"
   }
 }
 
